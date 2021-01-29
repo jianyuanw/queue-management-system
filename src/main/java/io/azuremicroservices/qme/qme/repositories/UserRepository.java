58e692072e527ea009de31cb8010679f05ece902
Cai Yaolong Derek <caiylderek@gmail.com>
2021-01-29 22:23:10 +0800

    Added Azure db MySQL settings, looking to add dependencies

--- src/main/java/io/azuremicroservices/qme/qme/repositories/UserRepository.java
+++ src/main/java/io/azuremicroservices/qme/qme/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package io.azuremicroservices.qme.qme.repositories;
 
-public interface UserRepository {
+import io.azuremicroservices.qme.qme.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }

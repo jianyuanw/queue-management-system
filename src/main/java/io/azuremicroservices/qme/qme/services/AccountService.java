8bbdc24e412b756355166e51135ac62549982091
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-02-05 17:48:08 +0800

    Implemented spring security, currently no restriction on who can access which page yet (to be done). Proper validation of confirm password at registration. Created temporary landing pages. Temporarily using CommandLineRunner to encode passwords seeded by data.sql for the following usernames: - client - appadmin - vendoradmin - branchadmin - branchoperator

--- src/main/java/io/azuremicroservices/qme/qme/services/AccountService.java
+++ src/main/java/io/azuremicroservices/qme/qme/services/AccountService.java
@@ -13,16 +13,28 @@ public class AccountService {
         this.userRepo = userRepo;
     }
 
+    public boolean usernameExists(String username) {
+        if (userRepo.findByUsername(username) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean emailExists(String email) {
+        if (userRepo.findByEmail(email) != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
+
     public void createClient(User user) {
         user.setRole(User.Role.CLIENT);
         userRepo.save(user);
-    }
-
-    public boolean verifyUser(String username, String password) {
-        User user = userRepo.findByUsername(username);
-        if (user != null && user.getPassword().equals(password)) {
-            return true;
-        }
-        return false;
     }
 }

d65af9b6c4f412bbbdc7a3e87f71080696886097
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-02-09 10:16:38 +0800

    Client landing page UI done.

--- src/main/java/io/azuremicroservices/qme/qme/controllers/ClientController.java
+++ src/main/java/io/azuremicroservices/qme/qme/controllers/ClientController.java
@@ -2,6 +2,7 @@ package io.azuremicroservices.qme.qme.controllers;
 
 import io.azuremicroservices.qme.qme.services.ClientService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,7 +17,17 @@ public class ClientController {
     }
 
     @GetMapping
-    public String clientLandingPage() {
+    public String clientLandingPage(Model model) {
+        // TODO: Retrieve categories from db instead of hardcode
+        String[] categories = {
+                "Healthcare",
+                "Finance",
+                "Dining",
+                "Technology",
+                "Retail",
+                "Government"
+        };
+        model.addAttribute("categories", categories);
         return "client/landing-page";
     }
 }

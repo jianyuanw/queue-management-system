c34960735774d0772010c7508e7e6e5ecee6263e
jianyuanw <69199572+jianyuanw@users.noreply.github.com>
2021-02-09 18:08:45 +0800

    Client search result UI done. To do search logic from landing page.

--- src/main/java/io/azuremicroservices/qme/qme/controllers/ClientController.java
+++ src/main/java/io/azuremicroservices/qme/qme/controllers/ClientController.java
@@ -1,10 +1,14 @@
 package io.azuremicroservices.qme.qme.controllers;
 
+import io.azuremicroservices.qme.qme.models.Branch;
 import io.azuremicroservices.qme.qme.services.ClientService;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/client")
@@ -29,5 +33,12 @@ public class ClientController {
         };
         model.addAttribute("categories", categories);
         return "client/landing-page";
+    }
+
+    @GetMapping("/search-result")
+    public String searchResult(Model model) {
+        List<Branch> branches = clientService.findBranchesBySearchTerm("splash");
+        model.addAttribute("branches", branches);
+        return "client/search-result";
     }
 }

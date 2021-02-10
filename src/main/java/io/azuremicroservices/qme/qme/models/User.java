98bbe0ecb307ead301a165f90abb68cfb30e140e
Cai Yaolong Derek <caiylderek@gmail.com>
2021-02-11 01:13:07 +0800

    Implemented branch operator view queues list and also made some changes
    - Previously forgot to add the add branch operator button into branch
    operator list
    
    - Removed displayName references in counter since displayName field was
    removed

--- src/main/java/io/azuremicroservices/qme/qme/models/User.java
+++ src/main/java/io/azuremicroservices/qme/qme/models/User.java
@@ -10,17 +10,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-import io.azuremicroservices.qme.qme.annotations.UniqueEmail;
-import io.azuremicroservices.qme.qme.annotations.UniqueUsername;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -54,6 +50,9 @@ public class User {
     
     private Role perspective;
 
+    @OneToOne(mappedBy = "user")
+    private Counter counter;
+    
     @OneToMany(mappedBy = "user")
     private List<SupportTicket> supportTickets;
     
--- src/main/java/io/azuremicroservices/qme/qme/services/QueuePositionService.java
+++ src/main/java/io/azuremicroservices/qme/qme/services/QueuePositionService.java
@@ -1,24 +1,33 @@
 package io.azuremicroservices.qme.qme.services;
 
-import io.azuremicroservices.qme.qme.models.Queue;
-import io.azuremicroservices.qme.qme.models.QueuePosition;
-import io.azuremicroservices.qme.qme.repositories.QueuePositionRepository;
-import io.azuremicroservices.qme.qme.repositories.QueueRepository;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
-import java.util.List;
+import io.azuremicroservices.qme.qme.models.Counter;
+import io.azuremicroservices.qme.qme.models.Queue;
+import io.azuremicroservices.qme.qme.models.QueuePosition;
+import io.azuremicroservices.qme.qme.models.QueuePosition.State;
+import io.azuremicroservices.qme.qme.repositories.CounterRepository;
+import io.azuremicroservices.qme.qme.repositories.QueuePositionRepository;
+import io.azuremicroservices.qme.qme.repositories.QueueRepository;
 
 @Service
 public class QueuePositionService {
     private final QueuePositionRepository queuePositionRepo;
     private final QueueRepository queueRepo;
     private final QueueService queueService;
+    private final CounterRepository counterRepo;
 
-    public QueuePositionService(QueuePositionRepository queuePositionRepo, QueueRepository queueRepo, QueueService queueService) {
+    public QueuePositionService(QueuePositionRepository queuePositionRepo, QueueRepository queueRepo, QueueService queueService, CounterRepository counterRepo) {
         this.queuePositionRepo = queuePositionRepo;
         this.queueRepo = queueRepo;
         this.queueService = queueService;
+        this.counterRepo = counterRepo;
     }
 
     public void updateReassignedIdPriority(Long reassignedId) {
@@ -72,4 +81,28 @@ public class QueuePositionService {
             }
         });
     }
+
+	public HashMap<Queue, Integer> findAllQueuePositionsInQueues(List<Queue> queuePermissions) {
+		HashMap<Queue, Integer> queueCount = new HashMap<>();
+		State[] activeStates = new State[] {State.ACTIVE_QUEUE, State.ACTIVE_REQUEUE};
+		for (Queue queue : queuePermissions) {
+			queueCount.put(queue, queuePositionRepo.countByQueue_IdAndStateIn(queue.getId(), activeStates));
+		}
+		
+		return queueCount;
+	}
+
+	public Counter findCounterByUserId(Long id) {
+		return counterRepo.findByUser_Id(id);		
+	}
+
+	public Map<Long, List<Counter>> findAllCountersByQueueId(List<Queue> queues) {
+		Map<Long, List<Counter>> queueCounters = new HashMap<>();
+		
+		for (Queue queue : queues) {
+			queueCounters.put(queue.getId(), counterRepo.findAllByQueue_IdAndUser_IdNull(queue.getId()));
+		}
+		
+		return queueCounters;
+	}
 }

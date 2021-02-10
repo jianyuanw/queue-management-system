93d5bab36cf81f72575b442d23f0bd5af9108754
Cai Yaolong Derek <caiylderek@gmail.com>
2021-02-10 11:48:56 +0800

    Switched stream call to native jpa query, implemented category
    search via client search function

--- src/main/java/io/azuremicroservices/qme/qme/controllers/ManageUserQueueController.java
+++ src/main/java/io/azuremicroservices/qme/qme/controllers/ManageUserQueueController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping ("manage/queue")
+@RequestMapping("manage/queue")
 public class ManageUserQueueController {
 
     private ManageUserQueueService queueService;
--- src/main/java/io/azuremicroservices/qme/qme/services/ClientService.java
+++ src/main/java/io/azuremicroservices/qme/qme/services/ClientService.java
@@ -1,12 +1,15 @@
 package io.azuremicroservices.qme.qme.services;
 
-import io.azuremicroservices.qme.qme.models.Branch;
-import io.azuremicroservices.qme.qme.repositories.BranchRepository;
-import io.azuremicroservices.qme.qme.repositories.QueueRepository;
-import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import io.azuremicroservices.qme.qme.models.Branch;
+import io.azuremicroservices.qme.qme.models.BranchCategory;
+import io.azuremicroservices.qme.qme.repositories.BranchRepository;
+import io.azuremicroservices.qme.qme.repositories.QueueRepository;
 
 @Service
 public class ClientService {
@@ -20,9 +23,26 @@ public class ClientService {
     }
 
     public List<Branch> findBranchesByQuery(String query) {
-        return branchRepo.findAll()
-                .stream()
-                .filter(x -> x.getName().toLowerCase().contains(query.toLowerCase()))
-                .collect(Collectors.toList());
+        return branchRepo.findAllByNameContaining(query);
     }
+
+	public List<Branch> findBranchesByCategory(String category) {
+		try {
+			BranchCategory branchCategory = Enum.valueOf(BranchCategory.class, category.toUpperCase());
+			return branchRepo.findAllByCategory(branchCategory);
+		} catch (IllegalArgumentException e) {
+			return new ArrayList<>();
+		}
+		
+	}
+
+	public List<Branch> findBranchesByQueryAndCategory(String query, String category) {
+		try {
+			BranchCategory branchCategory = Enum.valueOf(BranchCategory.class, category.toUpperCase());
+			return branchRepo.findAllByCategoryAndNameContaining(branchCategory, query);
+		} catch (IllegalArgumentException e) {
+			return new ArrayList<>();
+		}
+		
+	}
 }

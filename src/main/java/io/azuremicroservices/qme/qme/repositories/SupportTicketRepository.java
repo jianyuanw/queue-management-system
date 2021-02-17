fddda62808bf006f0aabdce3382944b804c358c4
Cai Yaolong Derek <caiylderek@gmail.com>
2021-02-17 22:01:24 +0800

    Separated client and admin support ticket interface
    - Changed view tickets to modal boxes

--- src/main/java/io/azuremicroservices/qme/qme/repositories/SupportTicketRepository.java
+++ src/main/java/io/azuremicroservices/qme/qme/repositories/SupportTicketRepository.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.azuremicroservices.qme.qme.models.SupportTicket;
+import io.azuremicroservices.qme.qme.models.SupportTicket.TicketState;
 import io.azuremicroservices.qme.qme.models.User;
 
 public interface SupportTicketRepository extends JpaRepository<SupportTicket, Long> {
 	List<SupportTicket> findAllByUser(User user);
+
+	List<SupportTicket> findAllByUserAndTicketState(User user, TicketState state);
 }
